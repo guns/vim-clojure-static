c95c328896b7eaa40e80849baf17d3f3530bc365
guns <self@sungpae.com>
2013-01-26 23:15:13 -0600

    Syntax keywords update
    * Replace test->, test->> with cond->, cond->>
    
    * Remove sequence functions from clojureRepeat, keeping only imperative
      looping keywords. Now the Repeat group actually conveys useful
      information
    
    * We are going to re-add dictionary completions for clojure.core and the
      special forms, so we add that logic here.
    
    * lein-exec will never use bleeding edge Clojure, so don't even bother
      with it.

--- vim_clojure_static.clj
+++ vim_clojure_static.clj
@@ -1,30 +1,31 @@
-#!/usr/bin/env lein-exec
-;;
-;; Vim keyword syntax generator for Clojure.
-;; Functionality kept in a monolithic function for easy copy and paste.
-;;
 ;; Copyright (c) 2012 Sung Pae <self@sungpae.com>
 ;; Distributed under the MIT license.
 ;; http://www.opensource.org/licenses/mit-license.php
+;;
+;; Copy this file to a project running the latest Clojure release and generate
+;; updated Vimscript definitions.
 
-(defn vim-syntax-keywords
-  "Return Vimscript literal syntax keyword definitions. Special forms,
-   constants, and every public var in clojure.core will be included."
-  []
+(ns vim-clojure-static)
+
+(def special-forms
+  "http://clojure.org/special_forms"
+  '[def if do let quote var fn loop recur throw try catch finally
+    monitor-enter monitor-exit . new set!])
+
+(def syntax-keywords
+  "Vimscript literal syntax keyword definitions. Special forms, constants, and
+   every public var in clojure.core is included."
   (let [builtins [["Constant" '[nil]]
                   ["Boolean" '[true false]]
-                  ;; http://clojure.org/special_forms
-                  ["Special" '[def if do let quote var fn loop recur throw try
-                               catch finally monitor-enter monitor-exit . new set!]]
+                  ["Special" special-forms]
                   ;; The duplicates from Special are intentional here
-                  ["Exception" '[try catch finally throw]]
-                  ["Cond" '[if-not if-let when when-> when->> when-not
-                            when-let when-first cond condp case]]
-                  ["Repeat" '[map mapv mapcat reduce reduce-kv filter filterv
-                              for doall dorun doseq dotimes map-indexed keep
-                              keep-indexed while]]]
+                  ["Exception" '[throw try catch finally]]
+                  ["Cond" '[case cond cond-> cond->> condp if-let if-not when
+                            when-first when-let when-not]]
+                  ;; Imperative looping constructs (not sequence functions)
+                  ["Repeat" '[doall dorun doseq dotimes while]]]
         declared (atom (set (filter symbol? (mapcat peek builtins))))
-        coresyms (keys (ns-publics 'clojure.core))
+        coresyms (keys (ns-publics `clojure.core))
         select! (fn [pred]
                   (let [xs (clojure.set/difference (set (filter pred coresyms)) @declared)]
                     (swap! declared into xs)
@@ -51,4 +52,23 @@
     (str "\" Clojure " (clojure-version) \newline
          (clojure.string/join \newline definitions))))
 
-(println (vim-syntax-keywords))
+(def core-dictionary
+  "Newline delimited string of public vars in clojure.core. Intended for use
+   as a dictionary file for Vim insert mode completion."
+  (->> `clojure.core
+       ns-publics
+       keys
+       (map str)
+       sort
+       (clojure.string/join \newline)))
+
+(def special-forms-dictionary
+  (->> special-forms
+       (map str)
+       sort
+       (clojure.string/join \newline)))
+
+(comment
+  (do (spit "/tmp/clojure-keywords.vim" syntax-keywords)
+      (spit "/home/guns/src/vim-clojure-static/ftplugin/clojure/clojure.core.txt" core-dictionary)
+      (spit "/home/guns/src/vim-clojure-static/ftplugin/clojure/special-forms.txt" special-forms-dictionary)))
