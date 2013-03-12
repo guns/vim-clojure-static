c5123ccc0c512ac356c413f936340086c01d1af2
guns <self@sungpae.com>
2013-03-12 14:44:34 -0500

    defsyntaxtest

--- clj/src/vim_clojure_static/test.clj
+++ clj/src/vim_clojure_static/test.clj
@@ -6,7 +6,8 @@
   (:require [clojure.java.io :as io]
             [clojure.java.shell :as shell]
             [clojure.edn :as edn]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.test :as test]))
 
 (defn syn-id-names
   "Map lines of clojure text to vim synID names at each column as keywords:
@@ -24,5 +25,56 @@
                 lines
                 (edn/read-string (peek (string/split-lines (slurp file)))))))
 
+(defn subfmt
+  "Extract a subsequence of seq s corresponding to the character positions of
+   %s in format spec fmt"
+  [fmt s]
+  (let [f (seq (format fmt \o001))
+        i (.indexOf f \o001)]
+    (->> s
+         (drop i)
+         (drop-last (- (count f) i 1)))))
+
+(defmacro defsyntaxtest
+  "Create a new testing var with tests in the format:
+
+   (defsyntaxtest example
+   (with-format \"#\\\"%s\\\"\"
+     \"123\" #(every? (partial = :clojureRegexp) %)
+     …)
+   (with-format …))
+
+   At runtime the syn-id-names of the strings (which are placed in the format
+   spec) are passed to their associated predicates. The format spec should
+   contain a single `%s`."
+  [name & body]
+  (assert (every? #(= 'with-format (first %)) body))
+  (assert (every? #(string? (second %)) body))
+  (assert (every? #(even? (count %)) body))
+  (let [[strings contexts] (reduce (fn [[strings contexts] [_ fmt & forms]]
+                                     (let [[ss λs] (apply map list (partition 2 forms))
+                                           ss (map #(format fmt %) ss)]
+                                       [(concat strings ss)
+                                        (conj contexts {:fmt fmt :ss ss :λs λs})]))
+                                   [[] []] body)]
+    `(test/deftest ~name
+       ;; Shellout to vim should happen at runtime
+       (let [~'synids (syn-id-names (str "tmp/" ~(str name) ".clj") ~@strings)]
+         ~@(map (fn [{:keys [fmt ss λs]}]
+                  `(test/testing ~fmt
+                     ~@(map (fn [s λ] `(test/is (~λ (subfmt ~fmt (get ~'synids ~s)))))
+                            ss λs)))
+                contexts)))))
+
 (comment
-  (syn-id-names "tmp/syntax.clj" "#\"^[a-z]$\""))
+
+  (macroexpand-1
+    '(defsyntaxtest number-literals-test
+       (with-format "%s"
+         "123" #(every? (partial = :clojureNumber) %)
+         "456" #(every? (partial = :clojureNumber) %))
+       (with-format "#\"%s\""
+         "^" #(= % [:clojureRegexpBoundary]))))
+  (test #'number-literals-test)
+
+  )
