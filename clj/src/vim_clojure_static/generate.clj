f39335c75cf9ccec000875346f1249356a415aa3
guns <self@sungpae.com>
2013-03-19 02:34:38 -0500

    Refector vim-clojure-static.generate
    * Separate source data from vimscript definitions and comments
    * \p{} expressions are referred to as "character property classes"
    * Do not match \p{IsjavaMethods}, since they are not a supported format
      of java.util.regex.Pattern, despite failing to raise Exceptions
    * Remove Unicode generation temporarily

--- clj/src/vim_clojure_static/generate.clj
+++ clj/src/vim_clojure_static/generate.clj
@@ -4,22 +4,51 @@
 (ns vim-clojure-static.generate
   (:require [clojure.string :as string]
             [clojure.set :as set]
-            [clojure.reflect :as r]))
+            [clojure.reflect :as reflect]))
 
-(def generation-message
-  (str "\" Generated from https://github.com/guns/vim-clojure-static/blob/vim-release-004/clj/src/vim_clojure_static/generate.clj"
-       \newline
-       "\" Clojure " (clojure-version)
-       \newline))
+;;
+;; Helpers
+;;
+
+(defn re-pattern?
+  "Returns true if s is a valid regular expression pattern, false otherwise."
+  [s]
+  (try
+    (re-pattern s)
+    true
+    (catch java.util.regex.PatternSyntaxException _ false)))
+
+(defn valid-property-class?
+  "Returns true if s is a valid regular expression character property class."
+  [s]
+  (re-pattern? (format "\\p{%s}" s)))
+
+(defn property-pattern
+  "Vimscript very magic pattern for a character property class."
+  [s]
+  (format "\\v\\\\[pP]\\{%s\\}" s))
+
+;;
+;; Definitions
+;;
+
+(def generation-comment
+  "\" Generated from https://github.com/guns/vim-clojure-static/blob/vim-release-004/clj/src/vim_clojure_static/generate.clj\n")
+
+(def clojure-version-comment
+  (format "\" Clojure version %s\n" (clojure-version)))
+
+(def java-version-comment
+  (format "\" Java version %s\n" (System/getProperty "java.version")))
 
 (def special-forms
   "http://clojure.org/special_forms"
   '[def if do let quote var fn loop recur throw try catch finally
     monitor-enter monitor-exit . new set!])
 
-(def syntax-keywords
-  "Vimscript literal syntax keyword definitions. Special forms, constants, and
-   every public var in clojure.core is included."
+(def keyword-groups
+  "Special forms, constants, and every public var in clojure.core listed by
+   syntax group suffix."
   (let [builtins [["Constant" '[nil]]
                   ["Boolean" '[true false]]
                   ["Special" special-forms]
@@ -34,14 +63,33 @@
         select! (fn [pred]
                   (let [xs (set/difference (set (filter pred coresyms)) @declared)]
                     (swap! declared into xs)
-                    xs))
-        builtins (conj builtins
-                       ;; Clojure devs are fastidious about accurate metadata
-                       ["Define" (select! #(re-seq #"\Adef(?!ault)" (str %)))]
-                       ["Macro" (select! #(:macro (meta (resolve %))))]
-                       ["Func" (select! #(:arglists (meta (resolve %))))]
-                       ["Variable" (select! identity)])
-        names (fn [coll]
+                    xs))]
+    (conj builtins
+          ;; Clojure devs are fastidious about accurate metadata
+          ["Define" (select! #(re-seq #"\Adef(?!ault)" (str %)))]
+          ["Macro" (select! #(:macro (meta (resolve %))))]
+          ["Func" (select! #(:arglists (meta (resolve %))))]
+          ["Variable" (select! identity)])))
+
+(def java-property-classes
+  "java.lang.Character boolean isMethods as valid regular expression property
+   class names."
+  (->> java.lang.Character
+       reflect/reflect
+       :members
+       (map (comp name :name))
+       (filter #(.startsWith % "is"))
+       distinct
+       (map #(string/replace % #"\Ais" "java"))
+       (filter valid-property-class?)))
+
+;;
+;; Vimscript literals
+;;
+
+(def vim-syntax-keywords
+  "Vimscript literal `syntax keyword` definitions."
+  (let [names (fn [coll]
                 (reduce (fn [v x]
                           ;; Include fully qualified versions of core vars
                           (cond (symbol? x) (if-let [m (meta (resolve x))]
@@ -51,14 +99,16 @@
                                 :else (conj v (str x))))
                         [] coll))
         definitions (map (fn [[group keywords]]
-                           (str "syntax keyword clojure" group \space
-                                (string/join \space (sort (names keywords)))))
-                         builtins)]
-    (str generation-message (string/join \newline definitions))))
+                           (format "syntax keyword clojure%s %s"
+                                   group
+                                   (string/join \space (sort (names keywords)))))
+                         keyword-groups)]
+    (str clojure-version-comment
+         (string/join \newline definitions))))
 
-(def completion-words
-  "Vimscript literal list of special forms and public vars in clojure.core."
-  (str generation-message
+(def vim-completion-words
+  "Vimscript literal list of words for omnifunc completion."
+  (str clojure-version-comment
        (format "let s:words = [%s]"
                (->> `clojure.core
                     ns-publics
@@ -68,75 +118,15 @@
                     sort
                     (string/join \,)))))
 
-;; Helper functions (should probably be moved to a util ns).
-
-(defn syntax-match [group pattern contained?]
-  "Returns a Vimscript literal `syntax match` statement. The content of pattern
-   is automatically wrapped in quotes."
-  (let [parts ["syntax match" (name group) (format "\"%s\"" pattern)]
-        parts (if contained?
-                (conj parts "contained")
-                parts)]
-    (string/join \space parts)))
-
-(defn re-pattern? [s]
-  "Returns true if s is a valid regular expression pattern, false otherwiese."
-  (try
-    (re-pattern s)
-    true
-    (catch java.util.regex.PatternSyntaxException _ false)))
-
-(defn pipe-join [ss]
-  (string/join \| ss))
-
-;;;; clojureRegex*CharClass generation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn bracket-class? [s]
-  "Returns true if s can be used in a bracket character class."
-  (re-pattern? (format "\\p{%s}" s)))
-
-;; This helps cut down on line noise.
-(defn bracket-class [s]
-  "Vimscript very magic regular expression for a bracket character class."
-  (format "\\v\\\\[pP]\\{%s\\}" s))
-
-(def unicode-char-classes
-  "Vimscript literal syntax match for unicode regex character classes."
-  (delay ;; Since we need to hit the network.
-    (let [block-data (slurp "http://www.unicode.org/Public/UNIDATA/Blocks.txt")
-          block-names (re-seq #"[A-F0-9]{4,6}\.\.[A-F0-9]{4,6}; (.+)" block-data)
-          bs1 (map #(string/replace (second %) #" " "") block-names)
-          bs2 (map #(string/replace (second %) #"[ -]" "_") block-names)
-          bs (sort (distinct (apply conj bs1 bs2)))
-          is-bs (filter #(bracket-class? (str "Is" %)) bs)
-          in-bs (filter #(bracket-class? (str "In" %)) bs)
-          is-cs (syntax-match
-                  :clojureRegexpUnicodeCharClass
-                  (bracket-class (format "Is%%(%s)" (pipe-join is-bs)))
-                  true)
-          in-cs (syntax-match
-                  :clojureRegexpUnicodeCharClass
-                  (bracket-class (format "In%%(%s)" (pipe-join in-bs)))
-                  true)]
-      (str is-cs "\n" in-cs))))
-
-(def java-char-classes
-  "Vimscript literal syntax match for (Is)java* regex character classes."
-  (let [is-methods (->> java.lang.Character
-                        r/reflect
-                        :members
-                        (map (comp name :name))
-                        (filter #(.startsWith % "is"))
-                        distinct
-                        sort)
-        cs (filter #(bracket-class? (str "java" %))
-                   (map #(second (string/split % #"is" 2)) is-methods))
-        {cs1 true cs2 false} (group-by #(bracket-class? (str "Is" %)) cs)]
-    (syntax-match
-      :clojureRegexpJavaCharClass
-      (bracket-class (format "%%(%%(Is)?java%%(%s)|java%%(%s))" (pipe-join cs1) (pipe-join cs2)))
-      true)))
+(def vim-java-char-classes
+  "Vimscript literal `syntax match` for \\p{javaMethod} property classes."
+  (let [ps (sort (map #(string/replace % #"\Ajava" "") java-property-classes))
+        vimpat (property-pattern (format "java%%(%s)" (string/join \| ps)))]
+    (str java-version-comment
+         (format "syntax match clojureRegexpJavaCharClass \"%s\" contained display" vimpat))))
 
 (comment
-  (spit "/tmp/clojure-defs.vim" (str syntax-keywords "\n\n" completion-words))
-  (spit "/tmp/clojure-char-classes.vim" (str java-char-classes "\n" @unicode-char-classes)))
+  (spit "tmp/clojure-defs.vim"
+        (->> [vim-syntax-keywords vim-completion-words vim-java-char-classes]
+             (map (partial str generation-comment))
+             (string/join "\n\n"))))
