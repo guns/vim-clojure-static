c4e68080207ebd589f7e481d79ee8f679665a300
guns <self@sungpae.com>
2013-09-07 16:21:03 -0500

    Replace %%VARS%% in update fn

--- clj/src/vim_clojure_static/update.clj
+++ clj/src/vim_clojure_static/update.clj
@@ -2,7 +2,9 @@
 
 (ns vim-clojure-static.update
   (:require [clojure.string :as string]
-            [clojure.java.shell :as shell]))
+            [clojure.java.shell :refer [sh]])
+  (:import (java.text SimpleDateFormat)
+           (java.util Date)))
 
 (defn update-vim!
   "Update Vim repository runtime files in dst/runtime"
@@ -11,7 +13,10 @@
         indent-file (join dst "runtime/doc/indent.txt")
         indent-buf (slurp indent-file)
         indent-match (re-find #"(?ms)^CLOJURE.*?(?=^[ \p{Lu}]+\t*\*)" indent-buf)
-        indent-doc (re-find #"(?ms)^CLOJURE.*(?=^ABOUT)" (slurp (join src "doc/clojure.txt")))]
+        indent-doc (re-find #"(?ms)^CLOJURE.*(?=^ABOUT)" (slurp (join src "doc/clojure.txt")))
+        current-date (.format (SimpleDateFormat. "dd MMMM YYYY") (Date.))
+        current-tag (string/trim-newline (:out (sh "git" "tag" "--points-at" "HEAD")))]
+    (assert (seq current-tag) "Git HEAD is not tagged!")
     ;; Insert indentation documentation
     (spit indent-file (string/replace-first indent-buf
                                             indent-match
@@ -20,8 +25,12 @@
     (doseq [file ["autoload/clojurecomplete.vim"
                   "ftplugin/clojure.vim"
                   "indent/clojure.vim"
-                  "syntax/clojure.vim"]]
-      (shell/sh "cp" (join src file) (join dst "runtime" file)))))
+                  "syntax/clojure.vim"]
+            :let [buf (-> (join src file)
+                          slurp
+                          (string/replace "%%RELEASE_DATE%%" current-date)
+                          (string/replace "%%RELEASE_TAG%%" current-tag))]]
+      (spit (join dst "runtime" file) buf))))
 
 (comment
   (update-vim! "/home/guns/src/vim-clojure-static" "/home/guns/src/vim"))
