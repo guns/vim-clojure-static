0a16fae45d5bea2486bf4b7bdbbf6866cdeccdce
guns <self@sungpae.com>
2014-02-16 03:17:57 -0600

    Update Vim patching functions

--- clj/src/vim_clojure_static/update.clj
+++ clj/src/vim_clojure_static/update.clj
@@ -1,36 +1,57 @@
 ;; Authors: Sung Pae <self@sungpae.com>
 
 (ns vim-clojure-static.update
-  (:require [clojure.java.shell :refer [sh]]
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
             [clojure.string :as string])
   (:import (java.text SimpleDateFormat)
            (java.util Date)))
 
+(defn fjoin [& args]
+  (string/join \/ args))
+
+(def clojure-section
+  #"(?ms)^CLOJURE.*?(?=^[\p{Lu} ]+\t*\*)")
+
+(defn update-doc! [first-line-pattern src-file dst-file]
+  (let [sbuf (->> src-file
+                  io/reader
+                  line-seq
+                  (drop-while #(not (re-find first-line-pattern %)))
+                  (string/join \newline))
+        dbuf (slurp dst-file)
+        dmatch (re-find clojure-section dbuf)
+        hunk (re-find clojure-section sbuf)]
+    (spit dst-file (string/replace-first dbuf dmatch hunk))))
+
+(defn copy-runtime-files! [src dst & opts]
+  (let [{:keys [tag date paths]} (apply hash-map opts)]
+    (doseq [path paths
+            :let [buf (-> (fjoin src path)
+                          slurp
+                          (string/replace "%%RELEASE_TAG%%" tag)
+                          (string/replace "%%RELEASE_DATE%%" date))]]
+      (spit (fjoin dst "runtime" path) buf))))
+
 (defn update-vim!
   "Update Vim repository runtime files in dst/runtime"
   [src dst]
-  (let [join (fn [& args] (string/join \/ args))
-        indent-file (join dst "runtime/doc/indent.txt")
-        indent-buf (slurp indent-file)
-        indent-match (re-find #"(?ms)^CLOJURE.*?(?=^[ \p{Lu}]+\t*\*)" indent-buf)
-        indent-doc (re-find #"(?ms)^CLOJURE.*(?=^ABOUT)" (slurp (join src "doc/clojure.txt")))
-        current-date (.format (SimpleDateFormat. "dd MMMM YYYY") (Date.))
-        current-tag (string/trim-newline (:out (sh "git" "tag" "--points-at" "HEAD")))]
+  (let [current-tag (string/trim-newline (:out (sh "git" "tag" "--points-at" "HEAD")))
+        current-date (.format (SimpleDateFormat. "dd MMMM YYYY") (Date.))]
     (assert (seq current-tag) "Git HEAD is not tagged!")
-    ;; Insert indentation documentation
-    (spit indent-file (string/replace-first indent-buf
-                                            indent-match
-                                            (str indent-doc \newline)))
-    ;; Copy runtime files
-    (doseq [file ["autoload/clojurecomplete.vim"
-                  "ftplugin/clojure.vim"
-                  "indent/clojure.vim"
-                  "syntax/clojure.vim"]
-            :let [buf (-> (join src file)
-                          slurp
-                          (string/replace "%%RELEASE_DATE%%" current-date)
-                          (string/replace "%%RELEASE_TAG%%" current-tag))]]
-      (spit (join dst "runtime" file) buf))))
+    (update-doc! #"CLOJURE\t*\*ft-clojure-indent\*"
+                 (fjoin src "doc/clojure.txt")
+                 (fjoin dst "runtime/doc/indent.txt"))
+    (update-doc! #"CLOJURE\t*\*ft-clojure-syntax\*"
+                 (fjoin src "doc/clojure.txt")
+                 (fjoin dst "runtime/doc/syntax.txt"))
+    (copy-runtime-files! src dst
+                         :tag current-tag
+                         :date current-date
+                         :paths ["autoload/clojurecomplete.vim"
+                                 "ftplugin/clojure.vim"
+                                 "indent/clojure.vim"
+                                 "syntax/clojure.vim"])))
 
 (comment
   (update-vim! "/home/guns/src/vim-clojure-static" "/home/guns/src/vim"))
