ebf231503410cf0223ca5c24e0522b58bc093b71
guns <self@sungpae.com>
2014-04-03 18:10:09 -0500

    Merge update ns into generate
    Separating them is inconvenient.

--- clj/src/vim_clojure_static/generate.clj
+++ clj/src/vim_clojure_static/generate.clj
@@ -2,13 +2,17 @@
 ;;          Joel Holdbrooks <cjholdbrooks@gmail.com>
 
 (ns vim-clojure-static.generate
-  (:require [clojure.set :as set]
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
+            [clojure.set :as set]
             [clojure.string :as string]
             [frak :as f])
   (:import (clojure.lang MultiFn)
            (java.lang Character$UnicodeBlock Character$UnicodeScript)
            (java.lang.reflect Field)
-           (java.util.regex Pattern$CharPropertyNames UnicodeProp)))
+           (java.text SimpleDateFormat)
+           (java.util Date)
+           (java.util.regex Pattern Pattern$CharPropertyNames UnicodeProp)))
 
 ;;
 ;; Helpers
@@ -290,7 +294,108 @@
   "Vimscript literal `setlocal lispwords=` statement."
   (str "setlocal lispwords=" (string/join \, (sort lispwords)) "\n"))
 
+;;
+;; Update functions
+;;
+
+(def CLOJURE-SECTION
+  #"(?ms)^CLOJURE.*?(?=^[\p{Lu} ]+\t*\*)")
+
+(defn fjoin [& args]
+  (string/join \/ args))
+
+(defn qstr [& xs]
+  (string/replace (apply str xs) "\\" "\\\\"))
+
+(defn update-doc! [first-line-pattern src-file dst-file]
+  (let [sbuf (->> src-file
+                  io/reader
+                  line-seq
+                  (drop-while #(not (re-find first-line-pattern %)))
+                  (string/join \newline))
+        dbuf (slurp dst-file)
+        dmatch (re-find CLOJURE-SECTION dbuf)
+        hunk (re-find CLOJURE-SECTION sbuf)]
+    (spit dst-file (string/replace-first dbuf dmatch hunk))))
+
+(defn copy-runtime-files! [src dst & opts]
+  (let [{:keys [tag date paths]} (apply hash-map opts)]
+    (doseq [path paths
+            :let [buf (-> (fjoin src path)
+                          slurp
+                          (string/replace "%%RELEASE_TAG%%" tag)
+                          (string/replace "%%RELEASE_DATE%%" date))]]
+      (spit (fjoin dst "runtime" path) buf))))
+
+(defn project-replacements [dir]
+  {(fjoin dir "syntax/clojure.vim")
+   {"-*- KEYWORDS -*-"
+    (qstr generation-comment
+          clojure-version-comment
+          vim-keywords)
+    "-*- CHARACTER PROPERTY CLASSES -*-"
+    (qstr generation-comment
+          java-version-comment
+          vim-posix-char-classes
+          vim-java-char-classes
+          vim-unicode-binary-char-classes
+          vim-unicode-category-char-classes
+          vim-unicode-script-char-classes
+          vim-unicode-block-char-classes)
+    "-*- TOP CLUSTER -*-"
+    (qstr generation-comment
+          (vim-top-cluster (mapv first keyword-groups)
+                             (slurp (fjoin dir "syntax/clojure.vim"))))}
+
+   (fjoin dir "ftplugin/clojure.vim")
+   {"-*- LISPWORDS -*-"
+    (qstr generation-comment
+          vim-lispwords)}
+
+   (fjoin dir "autoload/clojurecomplete.vim")
+   {"-*- COMPLETION WORDS -*-"
+    (qstr generation-comment
+          clojure-version-comment
+          vim-completion-words)}})
+
+(defn update-project!
+  "Update project runtime files in the given directory."
+  [dir]
+  (doseq [[file replacements] (project-replacements dir)]
+    (doseq [[magic-comment replacement] replacements]
+      (let [buf (slurp file)
+            pat (Pattern/compile (str "(?s)\\Q" magic-comment "\\E\\n.*?\\n\\n"))
+            rep (str magic-comment "\n" replacement "\n")
+            buf' (string/replace buf pat rep)]
+        (if (= buf buf')
+          (printf "No changes: %s\n" magic-comment)
+          (do (printf "Updating %s\n" magic-comment)
+              (spit file buf')))))))
+
+(defn update-vim!
+  "Update Vim repository runtime files in dst/runtime"
+  [src dst]
+  (let [current-tag (string/trim-newline (:out (sh "git" "tag" "--points-at" "HEAD")))
+        current-date (.format (SimpleDateFormat. "dd MMMM YYYY") (Date.))]
+    ; (assert (seq current-tag) "Git HEAD is not tagged!")
+    (update-doc! #"CLOJURE\t*\*ft-clojure-indent\*"
+                 (fjoin src "doc/clojure.txt")
+                 (fjoin dst "runtime/doc/indent.txt"))
+    (update-doc! #"CLOJURE\t*\*ft-clojure-syntax\*"
+                 (fjoin src "doc/clojure.txt")
+                 (fjoin dst "runtime/doc/syntax.txt"))
+    (copy-runtime-files! src dst
+                         :tag current-tag
+                         :date current-date
+                         :paths ["autoload/clojurecomplete.vim"
+                                 "ftplugin/clojure.vim"
+                                 "indent/clojure.vim"
+                                 "syntax/clojure.vim"])))
+
 (comment
+  (update-project! "..")
+  (update-vim! ".." "../../vim")
+
   ;; Generate an example file with all possible character property literals.
   (spit "tmp/all-char-props.clj"
         comprehensive-clojure-character-property-regexps)
