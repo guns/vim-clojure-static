29f11d20fd0b33df5ac93d2c170d574c78ee0b95
Richard Hull <rm_hull@yahoo.co.uk>
2016-04-23 20:15:04 +0100

    Update to clojure 1.8

--- clj/project.clj
+++ clj/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Vim License"
             :url "http://vimdoc.sourceforge.net/htmldoc/uganda.html#license"
             :comments ":help license"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [frak "0.1.6"]])
